2f4e7b0795fe60759ae7af8879810de2803dbe71
mutjin08 <mutjin08@gmail.com>
2025-02-02 20:50:05 +0900

    25-02-02

--- data_structure_2/14425_문자열 집합.py
+++ data_structure_2/14425_문자열 집합.py
@@ -1,16 +1,17 @@
+
 n, m = map(int, input().split())
+
 ss = []
 for _ in range(n):
     ss.append(input())
 
-cnt = 0
+answer = 0
 for _ in range(m):
-    target = input()
+    t = input()
     flag = 0
     for s in ss:
-        if target in s:
+        if t == s:
             flag = 1
             break
-    cnt += flag
-
-print(cnt)
+    answer += flag
+print(answer)
